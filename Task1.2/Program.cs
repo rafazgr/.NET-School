9c2781c5c7716dc4c1e2e8dea6b2be9365dfef0c
Rafael Zamora <111663412+rafazgr@users.noreply.github.com>
2023-11-02 20:58:31 -0600

    Task 1.2

---
+++ Task1.2/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static int CalculateCheckDigit(string digits)
+    {
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            int digit = int.Parse(digits[i].ToString());
+            sum += (10 - i) * digit;
+        }
+
+        // We simply add the remaining value to the number so that it is a multiple of 11
+        int checkDigit = 11 - (sum % 11);
+
+        return checkDigit;
+    }
+
+    static void Main()
+    {
+        string isbn;
+
+        Console.WriteLine("Enter the first 9 digits of the ISBN:");
+        string id = Console.ReadLine();
+
+        int checkDigit = CalculateCheckDigit(id);
+
+        if (checkDigit == 10)
+        {
+            isbn = id + "X";
+        }
+        else
+        {
+            isbn = id + checkDigit.ToString();
+        }
+
+        Console.WriteLine("The ISBN with the check digit is: " + isbn);
+    }
+}
