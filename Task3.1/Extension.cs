540f1323e4ff0f6ef61bad9bb551839ce12acd0c
Rafael Zamora <111663412+rafazgr@users.noreply.github.com>
2023-11-20 13:07:04 -0600

    Task 3.1

---
+++ Task3.1/Extension.cs
@@ -0,0 +1,22 @@
+﻿namespace QueueTask
+{
+    public static class QueueExtension
+    {
+        public static IQueue<T> Tail<T>(this IQueue<T> queue)
+        {
+            if (queue.IsEmpty())
+            {
+                throw new InvalidOperationException("Cannot get Tail of an empty queue.");
+            }
+
+            IQueue<T> newQueue = new Queue<T>();
+            queue.Dequeue();
+            while (!queue.IsEmpty())
+            {
+                newQueue.Enqueue(queue.Dequeue());
+            }
+
+            return newQueue;
+        }
+    }
+}
