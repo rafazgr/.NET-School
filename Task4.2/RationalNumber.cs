74d5a0e142fc30069613c80efec3876c60fbca96
Rafael Zamora <111663412+rafazgr@users.noreply.github.com>
2023-11-28 21:10:57 -0600

    Task 4.2

---
+++ Task4.2/RationalNumber.cs
@@ -0,0 +1,128 @@
+﻿namespace RationalNumberTask
+{
+    public sealed class RationalNumber : IComparable<RationalNumber>
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+
+        public RationalNumber(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.");
+            }
+
+            int gcd = GreatestCommonDivisor(Math.Abs(numerator), Math.Abs(denominator));
+            Numerator = numerator / gcd;
+            Denominator = denominator / gcd;
+
+            if (denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RationalNumber other)
+            {
+                return Numerator == other.Numerator && Denominator == other.Denominator;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public int CompareTo(RationalNumber other)
+        {
+            long thisValue = (long)Numerator * other.Denominator;
+            long otherValue = (long)other.Numerator * Denominator;
+            return thisValue.CompareTo(otherValue);
+        }
+
+        public static RationalNumber operator +(RationalNumber a, RationalNumber b)
+        {
+            if (a is null || b is null)
+            {
+                throw new ArgumentNullException("Operand cannot be null.");
+            }
+
+            int numerator = a.Numerator * b.Denominator + b.Numerator * a.Denominator;
+            int denominator = a.Denominator * b.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber a, RationalNumber b)
+        {
+            if (a is null || b is null)
+            {
+                throw new ArgumentNullException("Operand cannot be null.");
+            }
+
+            int numerator = a.Numerator * b.Denominator - b.Numerator * a.Denominator;
+            int denominator = a.Denominator * b.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        public static RationalNumber operator *(RationalNumber a, RationalNumber b)
+        {
+            if (a is null || b is null)
+            {
+                throw new ArgumentNullException("Operand cannot be null.");
+            }
+
+            int numerator = a.Numerator * b.Numerator;
+            int denominator = a.Denominator * b.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber a, RationalNumber b)
+        {
+            if (a is null || b is null)
+            {
+                throw new ArgumentNullException("Operand cannot be null.");
+            }
+
+            if (b.Numerator == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+
+            int numerator = a.Numerator * b.Denominator;
+            int denominator = a.Denominator * b.Numerator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        public static explicit operator double(RationalNumber rational)
+        {
+            return (double)rational.Numerator / rational.Denominator;
+        }
+
+        public static implicit operator RationalNumber(int value)
+        {
+            return new RationalNumber(value, 1);
+        }
+
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+    }
+}
