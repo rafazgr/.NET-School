0639d194c681a9335e2e429257f2712858699996
Rafael Zamora <111663412+rafazgr@users.noreply.github.com>
2023-12-01 01:02:26 -0600

    Task 5.1

---
+++ Task5.1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SparseMatrixTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            SparseMatrix matrix = new SparseMatrix(4, 5);
+            // SparseMatrix matrix2 = new SparseMatrix(-1, 0); // This will throw an exception
+
+            matrix[0, 1] = 5;
+            matrix[1, 2] = 8;
+            matrix[2, 0] = 3;
+            matrix[2, 3] = 2;
+            matrix[3, 1] = 5;
+            matrix[3, 3] = 5;
+            matrix[3, 4] = 7;
+            //matrix[4, 5] = 7; // This will throw an exception
+
+            Console.WriteLine(matrix);
+
+            Console.WriteLine("All Elements:");
+            foreach (var element in matrix)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine("\n\nNonzero Elements:");
+            foreach (var element in matrix.GetNonzeroElements())
+            {
+                Console.WriteLine($"({element.Item1}, {element.Item2}): {element.Item3}");
+            }
+
+            Console.WriteLine("\nCount of 0 in the matrix: " + matrix.GetCount(0));
+            Console.WriteLine("Count of 1 in the matrix: " + matrix.GetCount(1));
+            Console.WriteLine("Count of 3 in the matrix: " + matrix.GetCount(3));
+            Console.WriteLine("Count of 5 in the matrix: " + matrix.GetCount(5));
+        }
+    }
+}
---
+++ Task5.1/SparseMatrix.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace SparseMatrixTask
+{
+    public class SparseMatrix : IEnumerable<long>
+    {
+        private Dictionary<(int, int), long> matrix;
+        private int rows;
+        private int columns;
+
+        public SparseMatrix(int rows, int columns)
+        {
+            if (rows <= 0 || columns <= 0)
+                throw new ArgumentException("Rows and columns must be strictly greater than zero.");
+
+            this.rows = rows;
+            this.columns = columns;
+            matrix = new Dictionary<(int, int), long>();
+        }
+
+        private void VerifyIndex(int i, int j)
+        {
+            if (i < 0 || i >= rows || j < 0 || j >= columns)
+                throw new IndexOutOfRangeException("Invalid matrix indices.");
+        }
+
+        public long this[int i, int j]
+        {
+            get
+            {
+                VerifyIndex(i, j);
+
+                return matrix.TryGetValue((i, j), out var value) ? value : 0;
+            }
+            set
+            {
+                VerifyIndex(i, j);
+
+                if (value != 0)
+                    matrix[(i, j)] = value;
+                else if (matrix.ContainsKey((i, j)))
+                    matrix.Remove((i, j));
+            }
+        }
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    yield return this[i, j];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public IEnumerable<(int, int, long)> GetNonzeroElements()
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    if (matrix.TryGetValue((i, j), out long value))
+                    {
+                        yield return (i, j, value);
+                    }
+                }
+            }
+        }
+
+        public int GetCount(long x)
+        {
+            return x == 0 ? rows * columns - matrix.Count : matrix.Count(entry => entry.Value == x);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    resultBuilder.Append($"{this[i, j]} ");
+                }
+
+                resultBuilder.AppendLine();
+            }
+
+            return resultBuilder.ToString();
+        }
+
+    }
+}
