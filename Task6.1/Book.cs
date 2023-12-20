4d8a8cf28cd21785756ef841bf365acbaeb9dced
Rafael Zamora <111663412+rafazgr@users.noreply.github.com>
2023-12-19 20:58:31 -0600

    Update Book.cs

--- Task6.1/Book.cs
+++ Task6.1/Book.cs
@@ -1,8 +1,9 @@
-﻿namespace BookCatalogTask
+namespace BookCatalogTask
 {
     public class Book
     {
         private string _title;
+        private string _isbn;
 
         public string Title
         {
@@ -20,6 +21,27 @@
 
         public DateTime? PublicationDate { get; set; }
         public List<Author> Authors { get; set; }
-        public string ISBN { get; set; }
+
+        public string ISBN
+        {
+            get => _isbn;
+            set
+            {
+                string normalizedISBN = NormalizeAndValidateISBN(value);
+                _isbn = normalizedISBN;
+            }
+        }
+
+        private string NormalizeAndValidateISBN(string isbn)
+        {
+            string normalizedISBN = isbn.Replace("-", "");
+
+            if (normalizedISBN.Length != 13 || !System.Text.RegularExpressions.Regex.IsMatch(normalizedISBN, "^[0-9]+$"))
+            {
+                throw new ArgumentException("Invalid ISBN format. Must have 13 digits and contain only digits 0-9.");
+            }
+
+            return normalizedISBN;
+        }
     }
 }
